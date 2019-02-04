c19c7c7b79328deb032e014add6c26d63d4b0383
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-04 02:13:50 -0500

    refactor: Remove commented code

--- vagrant/restaurant_project/restaurant-app.py
+++ vagrant/restaurant_project/restaurant-app.py
@@ -13,13 +13,6 @@ def restaurantMenu(restaurant_id):
     session = DBSession()
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    # output = ''
-    # for i in items:
-    #     output += i.name + '<br>'
-    #     output += i.price + '<br>'
-    #     output += i.description + '<br>'
-    #     output += '<br>'
-    # return output
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
