af611f54ffa0bab4759454c34592dadaca52a93f
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-04 00:02:28 -0500

    feat: Add edit menu item functionality

--- vagrant/restaurant_project/restaurant-app.py
+++ vagrant/restaurant_project/restaurant-app.py
@@ -6,11 +6,15 @@ from database_setup import Base, Restaurant, MenuItem
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
-DBSession = sessionmaker(bind = engine)
-session = DBSession()
+
+def createSession():
+    DBSession = sessionmaker(bind = engine)
+    session = DBSession()
+    return session
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
+    session = createSession()
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     # output = ''
@@ -25,6 +29,7 @@ def restaurantMenu(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
     if request.method == 'POST':
+        session = createSession()
         newItem = MenuItem(name = request.form['name'], restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
@@ -32,9 +37,19 @@ def newMenuItem(restaurant_id):
     else:
         return render_template('new-menu-item.html', restaurant_id=restaurant_id)
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "Page to edit a menu item"
+    session = createSession()
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template(
+            'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
