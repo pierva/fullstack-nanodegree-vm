c70c3f49b5321773849751dd8ebad49efaa65d07
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-07 02:02:21 -0500

    chore: Update database setup

--- vagrant/restaurant-menu/database_setup.py
+++ vagrant/restaurant-menu/database_setup.py
@@ -1,4 +1,5 @@
 import sys
+from sqlalchemy.types import Numeric
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -7,32 +8,52 @@ from sqlalchemy import create_engine
 
 Base = declarative_base() # it creates a Base class instance
 
-
 class Restaurant(Base):
     __tablename__ = 'restaurant'
     id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False)
+    name = Column(String(50), nullable=False)
+    street = Column(String(95), nullable=True)
+    city = Column(String(40), nullable=False)
+    state = Column(String(2), nullable=False)
+    zip = Column(String(10), nullable=True)
+    lat = Column(String(10), nullable=True)
+    lon = Column(String(10), nullable=True)
 
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-    course = Column(String(250))
+    course = Column(String(30))
     description = Column(String(250))
-    price = Column(String(8))
+    price = Column(String(6))
+    picture = Column(String(250), nullable=True)
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
     @property
-    def serialize(self):
+    def serialize_restaurants(self):
         #Returns object data in easily serializeable format
         return {
-            'name' : self.name,
-            'description' : self.description,
-            'id' : self.id,
-            'price' : self.price,
-            'course' : self.course
+            'id': self.id,
+            'name': self.name,
+            'street': self.street,
+            'city': self.city,
+            'state': self.state,
+            'zip': self.zip,
+            'lat': self.lat,
+            'lon': self.lon
+        }
+
+    def serialize_menu(self):
+        return {
+            'restaurant': self.restaurant_id,
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'course': self.course,
+            'description': self.description,
+            'picture': self.picture
         }
 
 
