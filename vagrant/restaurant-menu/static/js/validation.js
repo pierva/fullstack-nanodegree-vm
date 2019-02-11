fa3306ee47eead6c57112538c95dee265b02a640
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-11 10:44:49 +0100

    fix: Fix add/edit restaurant validation problems

--- vagrant/restaurant-menu/static/js/validation.js
+++ vagrant/restaurant-menu/static/js/validation.js
@@ -1,12 +1,26 @@
 function checkCoordinates(){
-  if($("#coordinates").length){
-    var inputs = $(this).find('input');
-    if(inputs.eq(0).val() !== "" || inputs.eq(0).val() !== "")
-    $("#add-restaurant").attr('disabled', false);
+  if($(".coordinates").find('input').length === 2){
+    var inputs = $(".coordinates").find('input');
+    if(inputs.eq(0).val() === "" || inputs.eq(1).val() === ""){
+      $("#add-restaurant").attr('disabled', true);
+    } else {
+      var enteredVal = inputs.eq(0).val() + inputs.eq(1).val()
+      var matchLower = enteredVal.search(/[a-z]/g);
+      var matchUpper = enteredVal.search(/[A-Z]/g);
+      console.log(matchLower + matchUpper);
+      if (matchLower + matchUpper <= 0){
+        $("#add-restaurant").attr('disabled', false);
+      } else {
+        $("#add-restaurant").attr('disabled', true);
+      }
+    }
+  }
+  else {
+  $("#add-restaurant").attr('disabled', true);
   }
 }
 
-$('#coordinates input').on('keyup', function(){
+$('.coordinates input').on('keyup', function(){
   checkCoordinates();
 });
 
