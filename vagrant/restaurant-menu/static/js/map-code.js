3ef49422e62064cb343eefb54c1dc9d2823883e3
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-11 11:22:59 +0100

    fix: Show restaurant name on map marker

--- vagrant/restaurant-menu/static/js/map-code.js
+++ vagrant/restaurant-menu/static/js/map-code.js
@@ -1,5 +1,6 @@
 var map;
 var position = {lat: 25.8124647, lng: -80.3375188}
+var name = ""
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: position,
@@ -8,13 +9,15 @@ function initMap() {
   var marker = new google.maps.Marker({
     position: position,
     map: map,
-    title: "Here"
+    title: name
   });
 }
 
 $(".restaurant-container").on('click', ".show-on-map", function(){
   var lat = $(this).data("lat");
   var lng = $(this).data("lon");
-  position = {lat: lat, lng: lng}
+  var restaurant = $(this).data("name");
+  position = {lat: lat, lng: lng};
+  name = restaurant;
   initMap();
 })
