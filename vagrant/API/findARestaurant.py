4d7e4f35eef19995b040e32338d3861579b0ec56
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-24 16:46:38 -0500

    fix: Solve list out of index error

--- vagrant/API/findARestaurant.py
+++ vagrant/API/findARestaurant.py
@@ -24,7 +24,11 @@ def getRestaurant(mealType, latLon):
     r = requests.get(url, params=params)
     results = r.json()
     if results['meta']['code'] == 200:
-        return results['response']['venues'][0]
+        venues = results['response']['venues'][0]
+        if len(venues) > 0:
+            return results['response']['venues'][0]
+        else:
+            return 'No restaurants available.'
     else:
         return "Error code %s.\n %s" % (results['meta']['code'], results['meta']['errorDetail'])
 
@@ -39,14 +43,17 @@ def getVenuePicture(venueId):
     r = requests.get(url, params=params)
     results = r.json()
     if results['meta']['code'] == 200:
-        details = results['response']['photos']['items'][0]
-        return details['prefix'] + "300x300" + details['suffix']
+        details = results['response']['photos']['items']
+        if len(details) > 0:
+            return details[0]['prefix'] + "300x300" + details[0]['suffix']
+        else:
+            return 'No picture available.'
     else:
         return "Error code %s.\n %s" % (results['meta']['code'], results['meta']['errorDetail'])
 
 def findARestaurant(mealType, location):
     coord = getGeocodeLocation(location)
-    coordString = '{},{}'.format(coord[lat], coord[lng])
+    coordString = '{},{}'.format(coord['lat'], coord['lng'])
     restaurant = getRestaurant(mealType, coordString)
     picture = getVenuePicture(restaurant['id'])
     try:
@@ -54,7 +61,19 @@ def findARestaurant(mealType, location):
             print 'Restaurant Name: %s' % restaurant['name']
             print 'Restaurant Address: %s' % restaurant['location']['address']
             print 'Image: %s' % picture
+            print '\n'
         else:
-            print 'Somenthing when wrong. Unable to find a restaurant.'
+            print 'Somenthing when wrong. Unable to find a restaurant.\n'
     except Exception as e:
-        print 'Error while processing your request.'
+        print 'Error while processing your request.\n'
+
+if __name__ == '__main__':
+	findARestaurant("Pizza", "Tokyo, Japan")
+	findARestaurant("Tacos", "Jakarta, Indonesia")
+	findARestaurant("Tapas", "Maputo, Mozambique")
+	findARestaurant("Falafel", "Cairo, Egypt")
+	findARestaurant("Spaghetti", "New Delhi, India")
+	findARestaurant("Cappuccino", "Geneva, Switzerland")
+	findARestaurant("Sushi", "Los Angeles, California")
+	findARestaurant("Steak", "La Paz, Bolivia")
+	findARestaurant("Gyros", "Sydney, Australia")
