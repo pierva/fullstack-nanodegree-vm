cb5a3696a21d372965517726c797f6325997f427
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-24 15:18:39 -0500

    chore: Add handler for non ASCII characters

--- vagrant/API/geocode.py
+++ vagrant/API/geocode.py
@@ -1,6 +1,11 @@
 import json
 import requests
 import config
+import sys
+import codecs
+
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
 def getGeocodeLocation(address):
     url = 'https://maps.googleapis.com/maps/api/geocode/json?'
