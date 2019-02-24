da1998d55992cf086655e4dd7b682f3b4ab02b11
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-24 16:13:21 -0500

    refactor: Move getGeocodeLocation method into a separate module

--- vagrant/API/findARestaurant.py
+++ vagrant/API/findARestaurant.py
@@ -4,20 +4,10 @@ import config
 import sys
 import codecs
 
+from geocode import getGeocodeLocation
+
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
-
-def getGeocodeLocation(address):
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?'
-    params = {
-                'sensor': 'false',
-                'address': address,
-                'key' : cofig.GEOCODER_API_KEY
-             }
-    r = requests.get(url, params=params)
-    results = r.json()
-    location = results['results'][0]['geometry']['location']
-    return location
 
 def getRestaurant(mealType, latLon):
     url = 'https://api.foursquare.com/v2/venues/search?'
---
+++ vagrant/API/geocode.py
@@ -0,0 +1,15 @@
+import json
+import config
+import requests
+
+def getGeocodeLocation(address):
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    params = {
+                'sensor': 'false',
+                'address': address,
+                'key' : config.GEOCODER_API_KEY
+             }
+    r = requests.get(url, params=params)
+    results = r.json()
+    location = results['results'][0]['geometry']['location']
+    return location
