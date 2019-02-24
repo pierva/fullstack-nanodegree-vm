e91c38426dccab84ad56342bef9eb2e759a1b98e
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-24 15:37:50 -0500

    feat: Rename file and add method to retrieve restaurants from foursqaure api

--- vagrant/API/findARestaurant.py
+++ vagrant/API/findARestaurant.py
@@ -18,3 +18,18 @@ def getGeocodeLocation(address):
     results = r.json()
     location = results['results'][0]['geometry']['location']
     return location
+
+def getRestaurant(mealType, latLon):
+    url = 'https://api.foursquare.com/v2/venues/search?'
+    params = {
+                'client_id': config.foursquare_client_id,
+                'client_secret': config.foursquare_secret,
+                'v': 20180323,
+                'intent': 'browse',
+                'radius': 50000,
+                'll': latLon,
+                'query': mealType
+             }
+    r = requests.get(url, params=params)
+    results = r.json()
+    print results
